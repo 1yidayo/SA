1cfed3dcb4a33b27d5ebbe6f9c4768c6b56ec57f
000-ing <xiangling0806@gmail.com>
2025-03-27 00:13:29 +0800

    登入註冊後端

--- login.php
+++ login.php
@@ -1,18 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>社團企業媒合平台</title>
+</head>
 <?php
     session_start();
-    $Account = $_POST['Account'];
-    $Password = $_POST['Password'];
-    $role = $_POST['role'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $level = $_POST['level'];
 
-    $link = mysqli_connect('localhost', 'root', '', 'test1');
-    $sql = "select * from users where Account = '$Account' and Password = '$Password'";
+    $link = mysqli_connect('localhost', 'root', '', 'SA');
+    $sql = "select * from users where username = '$username' and password = '$password'";
     $result = mysqli_query($link, $sql);
     if($row = mysqli_fetch_assoc($result)){
-        $_SESSION['Account'] = $row['Account'];
-        $_SESSION['role'] = $row['role'];
-        if($row['role'] === 'club'){
-            header("Location: club.html");
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['level'] = $row['level'];
+        if($row['level'] === 'cl'){
+            header("Location: cl.html");
             exit();
+        } elseif($row['level'] === 'en') {
+            header("Location: en.html");
+            exit();
+        } elseif($row['level'] === 'ai') {
+            header("Location: ai.html");
+            exit();
+        } elseif($row['level'] === 'de') {
+            header("Location: de.html");
+            exit();
+        } elseif($row['level'] === 'sc') {
+            header("Location: sc.html");
         }
         else{
             echo "<center><h1>無效的身分</h1></center>";
@@ -21,3 +39,4 @@
         echo "<h1>登入失敗！！！</h1>";
     }
 ?>
+</html>
---
+++ register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>社團企業媒合平台</title>
+    <meta http-equiv = "refresh" content = "3; url = login.html">
+</head>
+<?php
+        $username = $_POST['username'] ??'';
+        $password = $_POST['password'] ??'';
+        $level = $_POST['level'] ??'';
+
+        $link = mysqli_connect('localhost', 'root', '', 'SA');
+        $sql = "insert into users (username, password, level) VALUES ('$username', '$password', '$level')";
+        if (mysqli_query($link, $sql))
+            {
+                echo "註冊成功";
+            }
+            else
+            {
+                echo "註冊失敗";
+            }
+
+            ini_set('display_errors', 1);
+            error_reporting(E_ALL);
+            
+    ?>
+</html>
