cf78580f6a3993c67321e1b29d2b4451da9c5bb5
000-ing <xiangling0806@gmail.com>
2025-03-31 15:12:35 +0800

    後端upload

---
+++ club_contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>社團企業媒合平台</title>
+</head>
+<body>
+    <?php
+        $money = $_POST['money'];
+        $people = $_POST['people'];
+        $year = $_POST['year'];
+        $type = $_POST['type'];
+        $upload = $_POST['upload'];
+        $ins = $_POST['ins'];
+
+        
+    ?>
+</body>
+</html>
