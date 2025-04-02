ce45d8bd7d1c7a5d6046f1b4a9180e1869562105
000-ing <xiangling0806@gmail.com>
2025-04-02 14:17:08 +0800

    企業發布後端

--- club.php
+++ club.php
@@ -138,7 +138,7 @@ https://templatemo.com/tm-591-villa-agency
                 <h4>企劃書內容<br><span>", $row['upload'], "</span></h4>
               </li>
               <li>
-                <h4><a href=", $row['ins'], " target=_blank>instagram</a><br><span></span></h4>
+                <h4><a href=", $row['ins'], " target=_blank>社群連結</a><br><span></span></h4>
               </li>
             </ul>
           </div>
---
+++ en_contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>社團企業媒合平台</title>
+    <meta http-equiv="refresh" content="3; url=club contact.html">
+</head>
+<body>
+    <?php
+        $money = $_POST['money'];
+        $enterprise = $_POST['enterprise'];
+        $type = $_POST['type'];
+        $code = $_POST['code'];
+        $ins = $_POST['ins'];
+        $title = $_POST['title'];
+        $information = $_POST['information'];
+
+        $link = mysqli_connect('localhost', 'root', '', 'SA');
+        $sql = "insert into club_requirements (money, enterprise, type, code, ins, title, information) 
+        VALUES ('$money', '$enterprise', '$type', '$code', '$ins', '$title', '$information')";
+        if (mysqli_query($link, $sql))
+            {
+                echo "發布成功";
+            }
+            else
+            {
+                echo "發布失敗";
+            }
+
+            ini_set('display_errors', 1);
+            error_reporting(E_ALL);
+    ?>
+</body>
+</html>
