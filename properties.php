ea0807f3376fd2359dc52f805c37402043726fd1
darrenk1201 <aa0986875107@gmail.com>
2025-04-05 13:00:10 +0800

    更改footer

--- properties.php
+++ properties.php
@@ -152,14 +152,12 @@ https://templatemo.com/tm-591-villa-agency
   </div>
 
   <footer>
-    <div class="container">
-      <div class="col-lg-12">
-        <p>Copyright © 2048 Villa Agency Co., Ltd. All rights reserved. 
-        
-        Design: <a rel="nofollow" href="https://templatemo.com" target="_blank">TemplateMo</a> Distribution: <a href="https://themewagon.com">ThemeWagon</a></p>
-      </div>
-    </div>
-  </footer>
+        <div class="container">
+            <div class="col-lg-8">
+                <p style="text-align: left; font-weight: bold;">社團企業媒合平台</p>
+            </div>
+        </div>
+    </footer>
 
   <!-- Scripts -->
   <!-- Bootstrap core JavaScript -->
