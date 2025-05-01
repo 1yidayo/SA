c467c18d523eb9c27809de8450ff910e8a950034
darrenk1201 <aa0986875107@gmail.com>
2025-05-01 15:58:36 +0800

    更新社團發布需求發布需求

--- club contact.php
+++ club contact.php
@@ -20,13 +20,7 @@
   <link rel="stylesheet" href="assets/css/owl.css">
   <link rel="stylesheet" href="assets/css/animate.css">
   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-  <!--
-
-TemplateMo 591 villa agency
-
-https://templatemo.com/tm-591-villa-agency
-
--->
+  
 </head>
 
 <body>
@@ -91,9 +85,12 @@ https://templatemo.com/tm-591-villa-agency
     <div class="row">
       <div class="col-lg-6" style="margin:auto">
         <form id="contact-form" action="club_contact1.php" method="post" enctype="multipart/form-data">
+
+
         <?php
         session_start();
         $link = mysqli_connect('localhost', 'root', '', 'SA');
+        
         $sql = "SELECT * FROM identity WHERE identityID = '" . $_SESSION['identityID'] . "'";
         $result = mysqli_query($link, $sql);
 
@@ -108,55 +105,14 @@ https://templatemo.com/tm-591-villa-agency
         <input type="hidden" name="identityID" value="<?php echo $_SESSION['identityID']; ?>">
           <div class="row">
             <div class="col-12">
-              <div class="input-group">
-                <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
-                  <span class="ms-1"><b>內文標題(必填)</b></span>
-                </div>
-                <input class="form-control" type="text" placeholder="請輸入內文標題" aria-label="Enter a City or Airport"
-                  name="title" value="" required title="必填欄位！">
-              </div>
-            </div>
-
-            <div class="row g-3">
-              <div class="col-12">
-                <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
-                  <span class="ms-1"><b>預算範圍(必填)</b></span>
-                </div>
-                <select class="form-select" aria-label="Default select example" name="money" required title="必填欄位！">
-                  <option value="">請選擇</option>
-                  <option value="$20,000以下">$20,000以下</option>
-                  <option value="$20,001-$30,000">$20,001-$30,000</option>
-                  <option value="$30,001-$50,000">$30,001-$50,000</option>
-                  <option value="$50,001-$70,000">$50,001-$70,000</option>
-                  <option value="$70,001以上">$70,001以上</option>
-                </select>
-              </div>
-
-              <div class="col-12">
                 <div class="input-group">
                   <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
-                    <span class="ms-1"><b>學校名稱(必填)</b></span>
+                    <span class="ms-1"><b>學校名稱</b></span>
                   </div>
                   <input class="form-control" type="text" placeholder="請輸入學校名稱(請輸入學校全名eg:輔仁大學)" aria-label="請輸入學校全名"
                     name="school" required value="<?php echo $school; ?>" >
                 </div>
-                <div class="input-group">
-                  <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
-                    <span class="ms-1"><b>預估規模(必填)</b></span>
-                  </div>
-                  <select class="form-control" type="text" placeholder="選擇預估規模人數" aria-label="請輸入學校全名" name="people"
-                    required>
-                    <option value="">請選擇</option>
-                    <option value="0-10人">0-10人</option>
-                    <option value="11-20人">11-20人</option>
-                    <option value="21-30人">21-30人</option>
-                    <option value="31-40人">31-40人</option>
-                    <option value="41-50人">41-50人</option>
-                    <option value="50人人以上">50人以上</option>
-                  </select>
-                </div>
-                <br>
-                <div class="input-group">
+            <div class="input-group">
                   <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
                     <span class="ms-1"><b>社團名稱</b></span>
                   </div>
@@ -176,12 +132,76 @@ https://templatemo.com/tm-591-villa-agency
                 <div class="col-12">
                   <div class="input-group">
                     <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
-                      <span class="ms-1"><b>活動類型(必填)</b></span>
+                      <span class="ms-1"><b>活動類型</b></span>
                     </div>
                     <input class="form-control" type="text" placeholder="請輸入活動類型(eg:表演型、學術型)"
-                      aria-label="Enter a City or Airport" name="type" required title="必填欄位！">
+                      aria-label="Enter a City or Airport" name="type" value="<?php echo $cltype; ?>" required title="必填欄位！">
                   </div>
                 </div>
+            <div class="col-12">
+              <div class="input-group">
+                <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
+                  <span class="ms-1"><b>內文標題(必填)</b></span>
+                </div>
+                <input class="form-control" type="text" placeholder="請輸入內文標題" aria-label="Enter a City or Airport"
+                  name="title" value="" required title="必填欄位！">
+                <div class="invalid-feedback">請填寫內文標題</div>
+              </div>
+            </div>
+            <div class="col-12">
+                <div class="input-group">
+                  <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
+                    <span class="ms-1"><b>需要的贊助類型(必填)</b></span>
+                  </div>
+                  <select class="form-select" name="support_type" id="support_type" required>
+                    <option value="">請選擇</option>
+                    <option value="金錢">金錢</option>
+                    <option value="物資">物資</option>
+                    <option value="場地">場地</option>
+                    <option value="提供實習">提供實習</option>
+                  </select>
+                </div>
+              </div>
+              
+            </div>
+            <br>
+            <br>
+
+            <div class="row g-3">
+              <div class="col-12">
+                <div class="col-12" id="money-group">
+                  <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
+                    <span class="ms-1"><b>預算範圍</b></span>
+                  </div>
+                  <select class="form-select" aria-label="Default select example" name="money" id="money" title="必填欄位！">
+                    <option value="">請選擇</option>
+                    <option value="$20,000以下">$20,000以下</option>
+                    <option value="$20,001-$30,000">$20,001-$30,000</option>
+                    <option value="$30,001-$50,000">$30,001-$50,000</option>
+                    <option value="$50,001-$70,000">$50,001-$70,000</option>
+                    <option value="$70,001以上">$70,001以上</option>
+                  </select>
+                </div>
+
+
+              
+                <div class="input-group">
+                  <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
+                    <span class="ms-1"><b>預估規模(必填)</b></span>
+                  </div>
+                  <select class="form-control" type="text" placeholder="選擇預估規模人數" aria-label="請輸入學校全名" name="people"
+                    required>
+                    <option value="">請選擇</option>
+                    <option value="0-10人">0-10人</option>
+                    <option value="11-20人">11-20人</option>
+                    <option value="21-30人">21-30人</option>
+                    <option value="31-40人">31-40人</option>
+                    <option value="41-50人">41-50人</option>
+                    <option value="50人人以上">50人以上</option>
+                  </select>
+                </div>
+                <br>
+                
                 <div class="col-12">
                   <div class="input-group">
                     <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
@@ -207,21 +227,7 @@ https://templatemo.com/tm-591-villa-agency
                   </div>
                 </div>
 
-                <div class="col-12">
-                  <div class="input-group">
-                    <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
-                      <span class="ms-1"><b>需要的贊助類型(必填)</b></span>
-                    </div>
-                    <select class="form-select" name="support_type" id="support_type" required>
-                      <option value="">請選擇</option>
-                      <option value="金錢">金錢</option>
-                      <option value="物資">物資</option>
-                      <option value="場地">場地</option>
-                      <option value="提供實習">提供實習</option>
-                    </select>
-                  </div>
-                </div>
-                <br>
+                
 
                 <div class="col-15">
                   <div class="input-group">
@@ -238,7 +244,7 @@ https://templatemo.com/tm-591-villa-agency
                       <span class="ms-1"><b>社群連結</b></span>
                     </div>
                     <input class="form-control" type="text" placeholder="請輸入相關社群連結"
-                      name="ins" value="<?php echo $clins; ?>" >
+                       name="ins" value="<?php echo $clins; ?>" >
                   </div>
                 </div>
 
@@ -247,17 +253,57 @@ https://templatemo.com/tm-591-villa-agency
                     <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
                       <span class="ms-1"><b>內文(必填)</b></span>
                     </div>
-                    <textarea class="form-control" name="information" id="information" placeholder="請輸入內文內容" require></textarea>
+                    <textarea class="form-control" name="information" id="information" placeholder="請輸入內文內容" required></textarea>
                   </div>
                 </div>
 
                 <div class="col-12">
                   <button class="btn btn-light w-100 py-2" type="submit"><b>發布</b></button>
+
                 </div>
               </div>
             </div>
         </form>
       </div>
+      <script>
+        (function () {
+          'use strict';
+
+          const form = document.querySelector('#contact-form');
+          const supportTypeSelect = document.getElementById('support_type');
+          const moneyGroup = document.getElementById('money-group');
+          const moneySelect = document.getElementById('money');
+
+          // 切換預算欄位顯示與必填狀態
+          function toggleMoneyField() {
+            if (supportTypeSelect.value === '金錢') {
+              moneyGroup.style.display = 'block';
+              moneySelect.setAttribute('required', 'required');
+            } else {
+              moneyGroup.style.display = 'none';
+              moneySelect.removeAttribute('required');
+              moneySelect.value = ""; // 清除選項
+            }
+          }
+
+          // 初始設定
+          toggleMoneyField();
+
+          // 綁定事件
+          supportTypeSelect.addEventListener('change', toggleMoneyField);
+
+          // 原本的 Bootstrap 驗證功能
+          form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        })();
+      </script>
+
+      
     </div>
   </div>
 </div>
