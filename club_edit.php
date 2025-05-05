bb659e0737ba3deb790550e9d79b5630583259c1
1yidayo <yi.412402232@gmail.com>
2025-05-06 00:28:10 +0800

    社團個資修改

--- club_edit.php
+++ club_edit.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <meta http-equiv="refresh" content="3; url=self.cl.php">
-</head>
-<body>
 <?php
 session_start();
 $school = $_POST['school'];
@@ -15,26 +6,31 @@ $clsize = $_POST['clsize'];
 $clyear = $_POST['clyear'];
 $cltype = $_POST['cltype'];
 $clins = $_POST['clins'];
+$clphone = $_POST['clphone'];
 $userID = $_SESSION['userID'];
 
 $link = mysqli_connect('localhost', 'root', '', 'SAS');
-$sql = "UPDATE identity SET school = '$school', club = '$club', clsize = '$clsize', 
-clyear = '$clyear', cltype = '$cltype', clins = '$clins' WHERE userID = '$userID'";
+mysqli_set_charset($link, "utf8"); // 設定編碼，避免中文亂碼
+
+$sql = "UPDATE identity 
+        SET school = '$school', 
+            club = '$club', 
+            clsize = '$clsize', 
+            clyear = '$clyear', 
+            cltype = '$cltype', 
+            clins = '$clins', 
+            clphone = '$clphone' 
+        WHERE userID = '$userID'";
 
 if (mysqli_query($link, $sql)) {
-    // 成功就跳出 alert 並跳轉
     echo "<script>
         alert('更新成功');
-        window.location.href = 'http://localhost/SA/self.cl.php';
+        window.location.href = 'self.cl.php';
     </script>";
 } else {
-    // 失敗也用 alert 顯示錯誤
     echo "<script>
-        alert('更新失敗');
-        window.location.href = 'http://localhost/SA/self.cl.php';
+        alert('更新失敗：" . mysqli_error($link) . "');
+        window.location.href = 'self.cl.php';
     </script>";
 }
 ?>
-
-    </body>
-</html>
