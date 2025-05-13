28583c6480a43c0bdf4ef655c0a6746c0dd8a9bf
Chou-1206 <412402347@m365.fju.edu.tw>
2025-05-13 15:58:15 +0800

    en的navbar再次修改

--- en_contact.php
+++ en_contact.php
@@ -31,7 +31,7 @@
             <ul class="nav">
               <li><a href="en.html">首頁</a></li>
               <li><a href="properties.php">瀏覽</a></li>
-              <li><a href="en_contact.php"  class="active">發布</a></li>
+              <li><a href="en_contact.php">發布</a></li>
               <li><a href="enhistory.php">發布歷史</a></li>
               <li><a href="self.en.php">個人頁面</a></li>
               <li><a href="login.html">登出</a></li>
