6d05edf3a7bc7e1e8b68e762d83b3bb96c7606f0
Chou-1206 <412402347@m365.fju.edu.tw>
2025-05-13 14:16:29 +0800

    Update enhistory.php

--- enhistory.php
+++ enhistory.php
@@ -125,12 +125,21 @@ https://templatemo.com/tm-591-villa-agency
                     <!-- ***** Menu Start ***** -->
                     <ul class="nav">
                             <li><a href="en.html" class="active">首頁</a></li>
+<<<<<<< Updated upstream
                             <li><a href="properties.php">瀏覽</a></li>
                             <li><a href="en_contact.php">發布</a></li>
                             <li><a href="enhistory.php">發布歷史</a></li>
                             <li><a href="self.en.php">個人頁面</a></li>
                             <li><a href="login.html">登出</a></li>
                             <li><a href="advanced search for enterprise.html"><i class="fa fa-calendar"></i>進階搜尋</a>
+=======
+                            <li><a href="properties2.php">瀏覽</a></li>
+                            <li><a href="club contact.php">發布</a></li>
+                            <li><a href="enhistory.php">發布歷史</a></li>
+                            <li><a href="self.en.php">個人頁面</a></li>
+                            <li><a href="first.html">登出</a></li>
+                            <li><a href="advanced search for enterprise.html"><i class="fa fa-calendar"></i>進階搜尋</ruby></a>
+>>>>>>> Stashed changes
                             </li>
                         </ul>
                     <a class='menu-trigger'>
